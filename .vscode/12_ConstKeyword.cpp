fe7a6de8fbfc6df6ab42da7b429131413cb20a50
RAmPAtel007 <codewizard0007@gmail.com>
2025-01-09 23:37:53 +0530

    first commit

---
+++ .vscode/12_ConstKeyword.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    // const int x=5;
+    // x=10;
+    // cout<<x;
+
+    //2. const with pointer 
+    // const int *a =new int(2);//CONST data, non const pointer 
+    // cout<<*a<<endl;
+    // int b=20;
+    // a=&b;  
+    // cout<<*a<<endl;
+    // return 0;
+
+    //3. const pointer, but non-const data
+
+    // int *const a = new int(2); 
+    // cout<<*a<<endl;
+    // *a=20;
+    // cout<<*a<<endl;
+    // int b=50;
+    // a=&b; //nahi chalega , const pointer 
+
+    // 4. const pointer , const data
+    // const int* const a= new int(10);
+    // cout<<*a<<endl;
+    // *a=50;
+    // int b=100;
+    // a = &b;
+    // return 0;
+
+
+}
